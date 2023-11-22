b3eafa8860fa0778e6fea75d8756f7031e92752f
Semyon Kharitonov <simonhool416@gmail.com>
2023-11-22 10:23:54 +0300

    qr payment

--- integration/src/main/java/ru/evotor/integration/entities/credentials/Credentials.kt
+++ integration/src/main/java/ru/evotor/integration/entities/credentials/Credentials.kt
@@ -9,32 +9,41 @@ import ru.evotor.integration.utils.writeVersioningData
 data class Credentials(
     val token: String?,
     val userId: String?,
-    val inn: String? = null
+    val inn: String? = null,
+    val qrId: Long? = null
 ) : KParcelable {
 
     override fun writeToParcel(parcel: Parcel, flags: Int) = with(parcel) {
         writeString(token)
         writeString(userId)
         writeString(inn)
-
-        writeVersioningData(VERSION) {}
+        writeVersioningData(VERSION) {
+            qrId?.let { writeLong(it) }
+        }
     }
 
     companion object {
-        private const val VERSION = 1
+        private const val VERSION = 2
 
-        @JvmField val CREATOR = parcelableCreator { parcel ->
+        @JvmField
+        val CREATOR = parcelableCreator { parcel ->
             with(parcel) {
                 val token = readString()
                 val userId = readString()
                 val inn = readString()
+                var qrId: Long? = null
 
-                readVersioningData(VERSION) { dataVersion -> }
+                readVersioningData(VERSION) { dataVersion ->
+                    if (dataVersion >= 2) {
+                        qrId = readLong()
+                    }
+                }
 
                 return@with Credentials(
                     token,
                     userId,
-                    inn
+                    inn,
+                    qrId
                 )
             }
         }
